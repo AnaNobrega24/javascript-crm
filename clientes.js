f3c71b93a6881c6c04c6286a048cdbdc72e59dfd
Ana nobrega <anakarolinefrnobrega@gmail.com>
2025-05-07 16:46:13 -0300

    criação de api.json

---
+++ clientes.js
@@ -0,0 +1,47 @@
+let clientes = [];
+
+function buscarClientes() {
+    fetch("http://localhost:3000/clientes")
+        .then((res) => res.json())
+        .then((lista) => {
+            clientes = lista;
+            carregarClientes(clientes);
+
+        })
+}
+buscarClientes();
+
+function carregarClientes(listaDeClientes) {
+    let tbodyElement = document.querySelector("#tabela");
+    tbodyElement.innerHTML = '';
+    listaDeClientes.map((cliente) => {
+        tbodyElement.innerHTML += `
+            <tr class="*:leading-[40px]">
+                <td>${cliente.nome}</td>
+                <td>${cliente.email}</td>
+                <td>${cliente.telefone}</td>
+                <td>${cliente.data}</td>
+                <td class="w-[100px] flex justify-center gap-4">
+                    <box-icon name="pencil"></box-icon>
+                    <box-icon name="trash"></box-icon>
+                </td>
+            </tr>
+        `;
+    })
+}
+
+carregarClientes(clientes);
+
+function cadastrarCliente(form){
+    event.preventDefault();
+
+    // vão pegar os valores dos inputs do formulario e transformar em um objeto
+    let formData = new FormData(form);
+    let cliente = Object.fromEntries(formData.entries());
+    
+    // inserir o novo cliente no final do arrray clientes
+    clientes.push(cliente);
+    sessionStorage.setItem("clientes", JSON.stringify(clientes));
+    mostrarOverlay();
+    carregarClientes(clientes);
+}
